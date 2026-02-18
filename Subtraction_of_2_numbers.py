e94033d3250dffd25f3a633528d75a2712b732d4
Akshaya CS <akshayacs2008@mail.com>
2026-02-18 22:07:47 +0530

    Finding Difference between two numbers

---
+++ Subtraction_of_2_numbers.py
@@ -0,0 +1,8 @@
+print("Subtraction of 2 numbers..")
+
+a = int(input("Enter the value of a :"))
+b = int(input("Enter the value og b :"))
+
+sub = a-b
+
+print("The Difference of ",a,"and",b,"is",sub)
