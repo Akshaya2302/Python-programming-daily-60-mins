14dac73776236227cc5e4e7af55b7bfb477f3bda
Akshaya2302 <akshayacs2008@gmail.com>
2026-03-10 23:21:24 +0530

    Storing information about the customers order and Calculating the total cost

---
+++ the_coffee_shop_receipt_program.py
@@ -0,0 +1,10 @@
+drink_name = input("Enter the Drink Name:")
+price = float(input("Enter the Price of a drink:$"))
+quantity = int(input("Enter the quantity:"))
+is_takeaway = True
+Total_Cost = price*quantity
+print("Drink Name:",drink_name)
+print("Price:",price)
+print("Quantity:",quantity)
+print("Takeaway:",is_takeaway)
+print("The Total Bill is:",Total_Cost)
