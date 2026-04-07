dc2f126e7e8c1c22ac730010f9ba9de8375ebdf0
Akshaya2302 <akshayacs2008@gmail.com>
2026-04-07 20:14:32 +0530

    Printing the output using dictionaries

---
+++ dict.py
@@ -0,0 +1,5 @@
+birthday = {"Alice": "Apr 1", "Bob": "Dec 12", "Carol": "Mar 4"}
+
+print("Alice's birthday is on " + birthday["Alice"])
+print("Bob's birthday is on " + birthday["Bob"])
+print( birthday["Carol"])
