bbb58b3801cd30a0f25422b91b685f45f09a261a
Akshaya2302 <akshayacs2008@gmail.com>
2026-04-08 23:49:56 +0530

    Practice program

---
+++ hello_github.py
@@ -0,0 +1,8 @@
+# A simple program to practice pushing to GitHub
+def main():
+    print("Hello, GitHub!")
+    print("This is my new Python program.")
+    print("Ready to be committed and pushed!")
+
+if __name__ == "__main__":
+    main()
