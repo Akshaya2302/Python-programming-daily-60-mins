ae70d29fc643dd52192dc1d8eb4e390e28938cf2
AKSHAYA C S <akshayacs2008@gmail.com>
2026-05-07 23:53:10 +0530

    Initialize main.py with greeting functionality

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    
+    print("GitHub Repository Initialized Successfully!")
+    name = input("Enter your name: ")
+    print(f"Welcome to the project, {name}!")
+
+if __name__ == "__main__":
+    main()
