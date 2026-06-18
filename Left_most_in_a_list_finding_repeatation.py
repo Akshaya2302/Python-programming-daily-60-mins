f9f8d0502f64accccf84cdabf0ac40b93ee8c396
AKSHAYA C S <akshayacs2008@gmail.com>
2026-06-18 23:44:52 +0530

    Finding the repeatation of a element in a list

--- Left_most_in_a_list_finding_repeatation.py
+++ Left_most_in_a_list_finding_repeatation.py
@@ -11,21 +11,3 @@ if not found:
     print("Not found")
 
 
-'''# 1. Define the list and the number we are looking for
-numbers = [5, 8, 2, 7, 8, 4]
-target = 8
-
-# 2. Set a flag to keep track if we found it
-found = False
-
-# 3. Loop through the list using the index
-for i in range(len(numbers)):
-    if numbers[i] == target:
-        print(f"Success! The leftmost {target} is at index {i}.")
-        found = True
-        break  # We stop here because we only want the FIRST (leftmost) one
-
-# 4. If the loop finishes and 'found' is still False, the number wasn't there
-if not found:
-    print("Sorry, that number is not in the list.")'''
-
